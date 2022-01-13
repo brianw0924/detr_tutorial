a323aae723a9b30cd2bfbf0c6cac2ad6fad5f7fb
brian <brianw0924@gmail.com>
2022-01-13 17:50:54 +0800

    skull

--- detr/datasets/__init__.py
+++ detr/datasets/__init__.py
@@ -4,7 +4,7 @@ import torchvision
 
 from .coco import build as build_coco
 from .face import build as build_face
-
+from .skull import build as build_skull
 
 def get_coco_api_from_dataset(dataset):
     for _ in range(10):
@@ -21,6 +21,8 @@ def build_dataset(image_set, args):
         return build_coco(image_set, args)
     if args.dataset_file == 'face':
         return build_face(image_set, args)
+    if args.dataset_file == 'skull':
+        return build_skull(image_set, args)
     if args.dataset_file == 'coco_panoptic':
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
